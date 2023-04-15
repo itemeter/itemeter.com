5c1b846b8af3f75e40908a74bc74d2386a52c35a
Nam Nguyen <me@namnd.com>
2023-04-15 11:36:06 +1000

    inital commit

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+aws s3 sync . s3://itemeter.com \
+  --exclude '*.DS_Store*' \
+  --exclude '*.sh' \
+  --exclude 'Session.vim'
+
+aws cloudfront create-invalidation \
+  --distribution-id "E2SFD9FX0N3KNR" \
+  --paths "/"
